bd99b42bc2d8fc65544d57432e6c8a38aa3fad31
Eliran Levi <levioctl@gmail.com>
2018-03-29 11:22:08 +0300

    Firefox extension can now send tabs list

--- firefox-tablister-extension/textual-switcher-agent.js
+++ firefox-tablister-extension/textual-switcher-agent.js
@@ -9,6 +9,21 @@ Listen for messages from the API proxy.
 console.log('registering callback for receive')
 port.onMessage.addListener((response) => {
   console.log("Received: " + response);
+  // List tabs
+  tabs = null
+  chrome.tabs.query({},
+    function(result) {
+      // Filter results to interesting keys
+      tabs = result.map(function(tab) {
+                          return {title: tab['title'],
+                                  id: tab['id'],
+                                  url: tab['url'],
+                                  favIconUrl: tab['favIconUrl']
+                                 };
+                        });
+      console.log('sending tab list');
+      port.postMessage(tabs);
+    });
 });
 
 console.log('registering onClicked')
