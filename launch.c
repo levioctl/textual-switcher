a01910aaf9c90c16289602a982eadb812424af5a
Eliran Levi <levioctl@gmail.com>
2018-04-03 12:37:22 +0300

    Bugfix: Change the PID file path location in the launcher as well

--- launch.c
+++ launch.c
@@ -10,16 +10,16 @@
 #include <sys/types.h>
 
 
-static const char *PID_FILE_PATH = "/run/lock/textual_switcher.pid";
+static const char *PID_FILE_PATH_TEMPLATE = "/run/user/%s/textual_switcher.pid";
 static const char *PYTHON_EXE_PATH = "/usr/bin/python";
 static const char *SCRIPT_PATH = "/usr/share/textual-switcher/switcher.py";
 
 
-void exec_switcher(void)
+void exec_switcher(char *pid_file_path)
 {
     char *switcher_argv[] = {(char*)PYTHON_EXE_PATH,
                              (char*)SCRIPT_PATH,
-                             (char*)PID_FILE_PATH,
+                             (char*)pid_file_path,
                              NULL};
     execvp(PYTHON_EXE_PATH, switcher_argv);
     exit(1);
@@ -27,29 +27,31 @@ void exec_switcher(void)
 
 int main(void)
 {
-    FILE *pid_file = fopen(PID_FILE_PATH, "r");
+    char pid_file_path[100];
+    snprintf((char*)&pid_file_path, sizeof(pid_file_path), PID_FILE_PATH_TEMPLATE, "1000");
+    FILE *pid_file = fopen(pid_file_path, "r");
     char pid_file_content[20];
     int pid = 0;
     const size_t pid_file_content_buffer_size = sizeof(pid_file_content) /
         sizeof(pid_file_content[0]);
     if (NULL == pid_file) {
-        exec_switcher();
+        exec_switcher(pid_file_path);
     }
     const size_t amount_read = fread(pid_file_content, 1,
         pid_file_content_buffer_size, pid_file);
     if (amount_read <= 0) {
-        exec_switcher();
+        exec_switcher(pid_file_path);
     }
     if (strlen(pid_file_content) == 0) {
-        exec_switcher();
+        exec_switcher(pid_file_path);
     }
     pid = atoi(pid_file_content);
     if (pid < 0) {
-        exec_switcher();
+        exec_switcher(pid_file_path);
     }
     const int result = kill(pid, SIGHUP);
     if (0 != result) {
-        exec_switcher();
+        exec_switcher(pid_file_path);
     }
     return 0;
 }
