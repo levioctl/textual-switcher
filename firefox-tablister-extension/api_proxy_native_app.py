63838d544863fe3afdd01b6c7df563dfe6d1edf5
Eliran Levi <levioctl@gmail.com>
2018-04-03 13:09:29 +0300

    Remove log prints from API proxy

--- firefox-tablister-extension/api_proxy_native_app.py
+++ firefox-tablister-extension/api_proxy_native_app.py
@@ -12,12 +12,6 @@ import select
 import struct
 
 
-def log(message):
-    import time
-    with open('/tmp/proxy.log', 'a') as f:
-        f.write(str(os.getpid()) + ': ' + time.ctime() + ': ' + str(message) + '\n')
-
-
 class ExtensionMessages(object):
     @staticmethod
     def in_fd():
@@ -47,7 +41,6 @@ class ExtensionMessages(object):
     # Send a message to stdout.
     @classmethod
     def send_message(cls, message):
-        log('sending message to extension')
         message = cls._encode_message(message)
         sys.stdout.write(message['length'])
         sys.stdout.write(message['content'])
@@ -67,22 +60,16 @@ class SwitcherMessages(object):
         self._out_fifo_filename = os.path.join(self.FIFO_PATH_DIR, self.OUT_FIFO_FILENAME)
         self._in_fifo = None
         self._connect()
-        log('done')
 
     def get_message(self):
-        log('reading data')
         data = os.read(self._in_fifo, 4096)
-        log('data read')
         if len(data) == 0:
-            log('data is empty')
             # Writer closed
             return None
         return data
 
     def send_message(self, message):
-        log('sending data to switcher')
         nr_bytes = os.write(self._out_fifo, message)
-        log('bytes sent: ' + str(nr_bytes))
 
     def in_fd(self):
         return self._in_fifo
@@ -92,13 +79,10 @@ class SwitcherMessages(object):
         self._connect()
 
     def _connect(self):
-        log('opening in fifo...')
         self._in_fifo = self._open_in_fifo()
-        log('opening out fifo...')
         try:
             self._out_fifo = self._open_out_fifo()
         except:
-            log('error opening out fifo')
             self.cleanup()
             raise
 
@@ -110,34 +94,24 @@ class SwitcherMessages(object):
                 raise
 
     def _open_in_fifo(self):
-        log('creating in fifo')
         self._create_fifo(self._in_fifo_filename)
-        log('opening in fifo...' + self._in_fifo_filename)
         fd = os.open(self._in_fifo_filename, os.O_RDONLY | os.O_NONBLOCK)
-        log('in fifo open.')
         return fd
 
     def _open_out_fifo(self):
-        log('creating out fifo')
         self._create_fifo(self._out_fifo_filename)
-        log('opening out fifo ' + self._out_fifo_filename)
         fd = os.open(self._out_fifo_filename, os.O_WRONLY)
-        log('out fifo open.')
         return fd
 
     @staticmethod
     def _create_fifo(fifo_path):
-        log('3')
         try:
             os.mkfifo(fifo_path)
         except OSError as ex:
-            log('4')
             if ex.errno != errno.EEXIST:
                 raise
-            log('fifo already exists.')
 
     def cleanup(self):
-        log('cleaning...')
         try:
             os.close(self._in_fifo)
         except:
@@ -176,17 +150,14 @@ class PointToPointPipesSwitch(object):
     def __init__(self, a, b):
         self._sides = dict(a=a, b=b)
         self._epoll = select.epoll()
-        log('registering...')
         self._epoll.register(self._sides['a'].in_fd())
         self._epoll.register(self._sides['b'].in_fd())
 
     def run(self):
         while True:
-            log('waiting for messages...')
             events = self._wait_for_events()
             for event in events:
                 if isinstance(event, Disconnection):
-                    log('Side %s has disconnected.' % (event.side,))
                     raise DisconnectionException(event.side)
                 elif isinstance(event, Message):
                     self._route_message(event)
@@ -202,20 +173,15 @@ class PointToPointPipesSwitch(object):
 
     def _wait_for_events(self):
         events = list()
-        log('polling...')
         for fd, event_type in self._epoll.poll():
             source = self._sides['a'] if fd == self._sides['a'].in_fd() else self._sides['b']
             if event_type in (select.EPOLLIN, select.EPOLLRDNORM):
-                log('message ready from ' +  str(source) + '. Reading...')
                 content = os.read(fd, 4096)
-                if isinstance(source, SwitcherMessages):
-                    log('Message content:' + content)
                 message = Message(content=content, source=source)
                 events.append(message)
             elif event_type == select.EPOLLHUP:
                 events.append(Disconnection(side=source))
             else:
-                log('bad event from fd '  +  str(fd) + ' of type ' +  str(event_type))
                 continue
 
         return events
@@ -232,18 +198,14 @@ def main():
             messageSwitch.run()
         except DisconnectionException as ex:
             if ex.side == switcher_messages:
-                log('Reconnecting...')
                 ex.side.reconnect()
-                log('Reconnected. Creating switch...')
                 messageSwitch = PointToPointPipesSwitch(a=extension_messages, b=switcher_messages)
-                log('Switch ready. Running switch...')
                 run_another_iteration = True
         except Exception as ex:
-            log('exception ', str(ex))
+            pass
         finally:
             if not run_another_iteration:
                 switcher_messages.cleanup()
-                log('DONE!!!')
 
 
 if __name__ == "__main__":
