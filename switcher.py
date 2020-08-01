5ce0b8734d35878c0c7bb450c6bd507bde4c354c
Eliran Levi <levioctl@gmail.com>
2020-08-01 10:27:37 +0300

    Separate switcher.py further by putting the search textbox in its own module

--- switcher.py
+++ switcher.py
@@ -16,12 +16,15 @@ import keycodes
 import window_entry
 import bookmark_store
 import entriestree
+import searchtextbox
 
 
 class DefaultApp:
-    def __init__(self, switcher_window):
+    def __init__(self, switcher_window, _entriestree):
         self._switcher_window = switcher_window
+        self._entriestree = _entriestree
         self._actions = {}
+        self._is_ctrl_pressed = False
 
         self._actions[keycodes.KEYCODE_ARROW_DOWN] = self._switcher_window.select_next_item
         self._actions[keycodes.KEYCODE_ARROW_UP] = self._switcher_window.select_previous_item
@@ -31,38 +34,71 @@ class DefaultApp:
         self._actions[keycodes.KEYCODE_K] = self._switcher_window.select_previous_item
         self._actions[keycodes.KEYCODE_C] = lambda: self._switcher_window.set_visible(False)
         self._actions[keycodes.KEYCODE_L] = self._refresh
-        self._actions[keycodes.KEYCODE_W] = lambda: self._switcher_window.search_textbox.set_text("")
-        self._actions[keycodes.KEYCODE_BACKSPACE] = lambda: self._switcher_window.send_signal_to_selected_process(signal.SIGTERM)
-        self._actions[keycodes.KEYCODE_BACKSLASH] = lambda: self._switcher_window.send_signal_to_selected_process(signal.SIGKILL)
+        self._actions[keycodes.KEYCODE_W] = lambda: self._switcher_window.empty_search_textbox()
+        self._actions[keycodes.KEYCODE_BACKSPACE] = self._term_selected_process
+        self._actions[keycodes.KEYCODE_BACKSLASH] = self._kill_selected_process
         self._actions[keycodes.KEYCODE_H] = self._switcher_window.toggle_help_next
         self._actions[keycodes.KEYCODE_CTRL_PLUS] = self._switcher_window.add_selection_as_bookmark
         #self._actions[keycodes.KEYCODE_CTRL_HYPEN] = self._switcher_window.remove_selected_bookmark
 
+    def _kill_selected_process(self):
+        if self._is_ctrl_pressed:
+            self._switcher_window.send_signal_to_selected_process(signal.SIGKILL)
+
+    def _term_selected_process(self):
+        if self._is_ctrl_pressed:
+            self._switcher_window.send_signal_to_selected_process(signal.SIGTERM)
+
     def _refresh(self):
         self._switcher_window.async_list_windows()
-        self.select_first_window()
+        self._entriestree.select_first_window()
 
     def _toggle_expanded_mode(self):
         self._switcher_window.expanded_mode = not self._switcher_window.expanded_mode
         self._switcher_window.enforce_expanded_mode()
 
+    def _terminate(self):
+        self._switcher_window.send_signal_to_selected_process(signal.SIGTERM)
+
     def handle_keypress(self, *args):
         keycode = args[1].get_keycode()[1]
         state = args[1].get_state()
-        #is_ctrl_pressed = (state & state.CONTROL_MASK).bit_length() > 0
+        self._is_ctrl_pressed = (state & state.CONTROL_MASK).bit_length() > 0
         # Don't switch focus in case of up/down arrow
         if keycode in self._actions:
             self._actions[keycode]()
 
+    def handle_entry_selection(self, *_):
+        record_type = self._entriestree.get_value_of_selected_row(entriestree.COL_NR_RECORD_TYPE)
+        if record_type in (entriestree.RECORD_TYPE_BROWSER_TAB, entriestree.RECORD_TYPE_WINDOW):
+            window_id = self._entriestree.get_value_of_selected_row(entriestree.COL_NR_WINDOW_ID)
+            if window_id is None:
+                return
+            try:
+                windowcontrol.focus_on_window(window_id)
+                # Setting the window to not visible causes Alt+Tab to avoid switcher (which is good)
+                self._switcher_window.set_visible(False)
+            except subprocess.CalledProcessError:
+                # Actual window list has changed since last reload
+                self.async_list_windows()
+            tab_id = self._entriestree.get_value_of_selected_row(entriestree.COL_NR_TAB_ID)
+            is_tab = tab_id >= 0
+            if is_tab:
+                window = self._switcher_window._windows[window_id]
+                self._switcher_window._tabcontrol.async_move_to_tab(tab_id, window.get_pid())
+        elif record_type == entriestree.RECORD_TYPE_BOOKMARK_ENTRY:
+            url = selected_window_id = self._entriestree.get_value_of_selected_row(entriestree.COL_NR_URL)
+            webbrowser.open(url)
+
 
 class EntriesSearch(DefaultApp):
-    def __init__(self, switcher_window):
-        DefaultApp.__init__(self, switcher_window)
+    def __init__(self, *args, **kwrags):
+        DefaultApp.__init__(self, *args, **kwrags)
 
 
 class ChooseParentBookmarksDir(DefaultApp):
-    def __init__(self, switcher_window):
-            DefaultApp.__init__(self, switcher_window)
+    def __init__(self, *args, **kwargs):
+            DefaultApp.__init__(self, *args, **kwargs)
 
 
 class EntryWindow(Gtk.Window):
@@ -80,18 +116,30 @@ class EntryWindow(Gtk.Window):
                       "Ctrl+H: Toggle Help")
     SHORT_HELP_TEXT = "Ctrl+H: Toggle Help"
 
+    def _entry_selected_callback(self, *_):
+        self._current_app.handle_entry_selection()
+
     def __init__(self):
         Gtk.Window.__init__(self, title=self.WINDOW_TITLE)
         self._xid = None
         self._windows = {}
         self._tabs = {}
         self._bookmarks = []
-        self._entriestree = entriestree.EntriesTree(self._window_selected_callback,
+
+        self._entriestree = entriestree.EntriesTree(self._entry_selected_callback,
                                                     self._treeview_keypress,
                                                     self._get_tab_icon_callback)
-        self.search_textbox = self._create_search_textbox()
-        self.select_first_window()
-        self._windowcontrol = windowcontrol.WindowControl()
+
+        # Gui Apps (different modes for the same window layout)
+        self._gui_apps = {'entries_search': EntriesSearch(self, self._entriestree),
+                          'choose_parent_bookmarks_dir_app': ChooseParentBookmarksDir(self, self._entriestree)
+        }
+        self._current_app = self._gui_apps['entries_search']
+
+        self._search_textbox = searchtextbox.SearchTextbox(self._current_app.handle_keypress,
+                                                           self._entry_selected_callback,
+                                                           self._entriestree)
+        self._entriestree.select_first_window()
         self._tabcontrol = tabcontrol.TabControl(self._update_tabs_callback, self._tab_icon_ready)
         glib_wrappers.register_signal(self._focus_on_me, signal.SIGHUP)
         self._set_window_properties()
@@ -100,15 +148,10 @@ class EntryWindow(Gtk.Window):
                                                               lambda: None)
         self._help_label = self._create_help_label()
         self._status_label = self._create_status_label()
+
         self._add_gui_components_to_window()
         self.async_list_windows()
         self.expanded_mode = True
-
-        # Gui Apps (different modes for the same window layout)
-        self._gui_apps = {'entries_search': EntriesSearch(self),
-                          'choose_parent_bookmarks_dir_app': ChooseParentBookmarksDir(self)
-        }
-        self._current_app = self._gui_apps['entries_search']
 
     def move_to_gui_app(self, gui_app_name):
         self._current_app = self._gui_apps[gui_app_name]
@@ -119,19 +162,11 @@ class EntryWindow(Gtk.Window):
     def _add_gui_components_to_window(self):
         vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
         self.add(vbox)
-        vbox.pack_start(self.search_textbox, expand=False, fill=True, padding=0)
+        vbox.pack_start(self._search_textbox.element, expand=False, fill=True, padding=0)
         treeview_scroll_wrapper = self._create_treeview_scroll_wrapper()
         vbox.pack_start(treeview_scroll_wrapper, True, True, 0)
         vbox.pack_start(self._status_label, False, True, 0)
         vbox.pack_start(self._help_label, False, True, 0)
-
-    def _create_search_textbox(self):
-        search_textbox = Gtk.Entry()
-        search_textbox.set_text("")
-        search_textbox.connect("changed", self._text_changed_callback)
-        search_textbox.connect("key-press-event", self._entry_keypress_callback)
-        search_textbox.connect("activate", self._window_selected_callback)
-        return search_textbox
 
     def _create_treeview_scroll_wrapper(self):
         scrollable_treeview = Gtk.ScrolledWindow()
@@ -153,10 +188,10 @@ class EntryWindow(Gtk.Window):
 
     def _focus_on_me(self):
         self.set_visible(True)
-        self._windowcontrol.async_focus_on_window(self._get_xid())
+        windowcontrol.async_focus_on_window(self._get_xid())
         self.async_list_windows()
 
-        self.search_textbox.set_text("")
+        self._search_textbox.element.set_text("")
 
     def _get_xid(self):
         if self._xid is None:
@@ -193,7 +228,7 @@ class EntryWindow(Gtk.Window):
         self._entriestree.refresh(self._windows, self._tabs, self._bookmarks, self.expanded_mode)
 
     def async_list_windows(self):
-        self._windowcontrol.async_list_windows(callback=self._update_windows_listbox_callback)
+        windowcontrol.async_list_windows(callback=self._update_windows_listbox_callback)
         self._status_label.set_text("Bookmarks: Reading from drive...")
         self._bookmarks_store.async_list_bookmarks()
 
@@ -250,53 +285,10 @@ class EntryWindow(Gtk.Window):
                         break
         self._entriestree.treeview.set_cursor(current.path)
 
-    def _entry_keypress_callback(self, *args):
-        self._current_app.handle_keypress(*args)
-
     def _treeview_keypress(self, *args):
         keycode = args[1].get_keycode()[1]
         if keycode not in (keycodes.KEYCODE_ARROW_UP, keycodes.KEYCODE_ARROW_DOWN):
-            self.search_textbox.grab_focus()
-
-    def _is_some_window_selected(self):
-        _, _iter = self._entriestree.get_selected_row()
-        return _iter is not None
-
-    def _window_selected_callback(self, *_):
-        record_type = self._entriestree.get_value_of_selected_row(entriestree.COL_NR_RECORD_TYPE)
-        if record_type in (entriestree.RECORD_TYPE_BROWSER_TAB, entriestree.RECORD_TYPE_WINDOW):
-            window_id = self._entriestree.get_value_of_selected_row(entriestree.COL_NR_WINDOW_ID)
-            if window_id is None:
-                return
-            try:
-                self._windowcontrol.focus_on_window(window_id)
-                # Setting the window to not visible causes Alt+Tab to avoid switcher (which is good)
-                self.set_visible(False)
-            except subprocess.CalledProcessError:
-                # Actual window list has changed since last reload
-                self.async_list_windows()
-            tab_id = self._entriestree.get_value_of_selected_row(entriestree.COL_NR_TAB_ID)
-            is_tab = tab_id >= 0
-            if is_tab:
-                window = self._windows[window_id]
-                self._tabcontrol.async_move_to_tab(tab_id, window.get_pid())
-        elif record_type == entriestree.RECORD_TYPE_BOOKMARK_ENTRY:
-            url = selected_window_id = self._entriestree.get_value_of_selected_row(entriestree.COL_NR_URL)
-            webbrowser.open(url)
-
-    def _text_changed_callback(self, search_textbox):
-        search_key = search_textbox.get_text()
-        search_key = search_key.decode('utf-8')
-        self._entriestree.update_search_key(search_key)
-        self._entriestree.treefilter.refilter()
-        if not self._is_some_window_selected():
-            self.select_first_window()
-        self.enforce_expanded_mode()
-        if len(self._entriestree.tree):
-            self._entriestree.select_first_tab_under_selected_window()
-
-    def select_first_window(self):
-        self._entriestree.select_first_window()
+            self._search_textbox.element.grab_focus()
 
     def send_signal_to_selected_process(self, signal_type):
         window_id = self._entriestree.get_value_of_selected_row(entriestree.COL_NR_WINDOW_ID)
@@ -309,6 +301,9 @@ class EntryWindow(Gtk.Window):
             self._help_label.set_text(self.FULL_HELP_TEXT)
         else:
             self._help_label.set_text(self.SHORT_HELP_TEXT)
+
+    def empty_search_textbox(self):
+        self._search_textbox.element.set_text("")
 
     def _connected_to_cloud_callback(self):
         print("Connected to cloud")
--- windowcontrol.py
+++ windowcontrol.py
@@ -8,63 +8,65 @@ import glib_wrappers
 import window
 
 
-class WindowControl(object):
-    LIST_WINDOWS_COMMAND = ["wmctrl", "-lpx"]
+LIST_WINDOWS_COMMAND = ["wmctrl", "-lpx"]
 
-    def async_list_windows(self, callback):
-        stdout = glib_wrappers.async_run_subprocess(self.LIST_WINDOWS_COMMAND)
-        io = GLib.IOChannel(stdout)
 
-        def list_windows_callback(*_, **__):
-            output = io.read()
-            windows = self.parse_wlist_output(output)
-            icons = self._get_icons()
-            for _window in windows:
-                _window.icon = icons.get(_window.xid, None)
-            callback(windows)
+def _get_icons():
+    screen = Wnck.Screen.get_default()
+    # The following is needed in order to wait for windows to ready
+    while Gtk.events_pending():
+        Gtk.main_iteration()
+    screen.force_update()
+    icons = {w.get_xid(): w.get_icon() for w in screen.get_windows()}
+    return icons
 
-        io.add_watch(GLib.IO_IN | GLib.IO_HUP, list_windows_callback)
 
-    @staticmethod
-    def focus_on_window(window_id):
-        window_id = hex(window_id)
-        command = ["wmctrl", "-ia", window_id]
-        glib_wrappers.async_run_subprocess(command)
+def async_list_windows(callback):
+    stdout = glib_wrappers.async_run_subprocess(LIST_WINDOWS_COMMAND)
+    io = GLib.IOChannel(stdout)
 
-    def async_focus_on_window(self, window_id):
-        window_id = hex(window_id)
-        command = ["wmctrl", "-iR", window_id]
-        glib_wrappers.async_run_subprocess(command)
+    def list_windows_callback(*_, **__):
+        output = io.read()
+        windows = parse_wlist_output(output)
+        icons = _get_icons()
+        for _window in windows:
+            _window.icon = icons.get(_window.xid, None)
+        callback(windows)
 
-    @staticmethod
-    def parse_wlist_output(wlist_output):
-        windows = list()
-        for line in wlist_output.splitlines():
-            _window = window.Window()
-            xid_hex_str, line = line.split(" ", 1)
-            _window.xid = int(xid_hex_str, 16)
-            line = line.lstrip()
-            _window.desktop_id, line = line.split(" ", 1)
-            line = line.lstrip()
-            pid_str, line = line.split(" ", 1)
-            _window.pid = int(pid_str)
-            line = line.lstrip()
-            _window.wm_class, line = line.split(" ", 1)
-            if _window.wm_class == "N/A":
-                continue
-            line = line.lstrip()
-            _window.hostname, line = line.split(" ", 1)
-            if _window.title == "Desktop":
-                continue
-            _window.title = line.lstrip()
-            windows.append(_window)
-        return windows
+    io.add_watch(GLib.IO_IN | GLib.IO_HUP, list_windows_callback)
 
-    def _get_icons(self):
-        screen = Wnck.Screen.get_default()
-        # The following is needed in order to wait for windows to ready
-        while Gtk.events_pending():
-            Gtk.main_iteration()
-        screen.force_update()
-        icons = {w.get_xid(): w.get_icon() for w in screen.get_windows()}
-        return icons
+
+def focus_on_window(window_id):
+    window_id = hex(window_id)
+    command = ["wmctrl", "-ia", window_id]
+    glib_wrappers.async_run_subprocess(command)
+
+
+def async_focus_on_window(window_id):
+    window_id = hex(window_id)
+    command = ["wmctrl", "-iR", window_id]
+    glib_wrappers.async_run_subprocess(command)
+
+
+def parse_wlist_output(wlist_output):
+    windows = list()
+    for line in wlist_output.splitlines():
+        _window = window.Window()
+        xid_hex_str, line = line.split(" ", 1)
+        _window.xid = int(xid_hex_str, 16)
+        line = line.lstrip()
+        _window.desktop_id, line = line.split(" ", 1)
+        line = line.lstrip()
+        pid_str, line = line.split(" ", 1)
+        _window.pid = int(pid_str)
+        line = line.lstrip()
+        _window.wm_class, line = line.split(" ", 1)
+        if _window.wm_class == "N/A":
+            continue
+        line = line.lstrip()
+        _window.hostname, line = line.split(" ", 1)
+        if _window.title == "Desktop":
+            continue
+        _window.title = line.lstrip()
+        windows.append(_window)
+    return windows
